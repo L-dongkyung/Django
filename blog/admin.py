991dd3789c4478d0f162aee219337602b29c08d1
User <ckldk91m@naver.com>
2021-03-31 16:00:13 +0900

    3.31

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'modify_dt', 'content')
+    list_filter = ('id', 'title', 'modify_dt',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+# admin.site.register(Post, PostAdmin)
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,26 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+'''
+
 from bookmark.views import BookmarkLV, BookmarkDV
 from django.views.generic import ListView, DetailView
 from bookmark.models import Bookmark
-
+'''
 from django.conf.urls import url
 from bookmark import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # class-based views
-    path('bookmark/', BookmarkLV.as_view(), name='index'),
-    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 
-    # urls with view definition
+
+
+
+    # class-based views (모델.py에 클래스나 함수 정의 후 불러오기)
+    # path('bookmark/', BookmarkLV.as_view(), name='index'),
+    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+
+    # urls with view definition (모델.py 정의 없이 바로 뷰에서 흐름 입력)
     # path('bookmark/', ListView.as_view(model=Bookmark), name='index'),
     # path('bookmark/<int:pk>/', DetailView.as_view(model=Bookmark), name='detail'),
 
-    # -blog- ------------------------------------------------------
 
 
 ]
