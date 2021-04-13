fc8ffbfa044002274c144b8ca9dc6d735d252b2d
dongkyung <ckldk91m@naver.com>
2021-04-13 17:44:07 +0900

    4.13

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class PostLV(ListView):
     model = Post
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
 
 # ---Detail view
