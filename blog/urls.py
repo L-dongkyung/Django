0f6747b956381fc99940e0673045d015f9428780
L-dongkyung <ckldk91m@naver.com>
2021-08-29 16:48:54 +0900

    8.29

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,26 @@ from blog import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.PostLV.as_view(), name='index'),    # /blog/
-    path('post/', views.PostLV.as_view(), name='post_index'),    # /blog/post/
-    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),   # /blog/post/django-example/
-    path('archive/', views.PostAV.as_view(), name='post_archive'),   # /blog/archive   --7
-    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),  # /blog/archive/2019/
-    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),  # /blog/archive/2019/nov/
-    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'), # /blog/archive/2019/nov/10/
-    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'), # /blog/archive/today/
+        # /blog/
+    path('', views.PostLV.as_view(), name='index'),
+        # /blog/post/
+    path('post/', views.PostLV.as_view(), name='post_index'),
+        # /blog/post/django-example/
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
+        # /blog/archive   --7
+    path('archive/', views.PostAV.as_view(), name='post_archive'),
+        # /blog/archive/2019/
+    path('archive/<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
+        # /blog/archive/2019/nov/
+    path('archive/<int:year>/<str:month>/', views.PostMAV.as_view(), name='post_month_archive'),
+        # /blog/archive/2019/nov/10/
+    path('archive/<int:year>/<str:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
+        # /blog/archive/today/
+    path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
+
+        # /blog/tag/
+    path('tag/', views.TagCloudTV.as_view(), name='tag_cloud'),
+        # /blog/tag/tagname/
+    path('tag/<str:tag>/', views.TaggedObjectLV.as_view(), name='tagged_object_list'),
+
 ]
