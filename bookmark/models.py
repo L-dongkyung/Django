140034a155c3e3185029ce4a7caa6d1a20d44407
L-dongkyung <ckldk91m@naver.com>
2021-09-05 15:01:10 +0900

    9.05 Post,bookmark CRUD

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,6 +9,7 @@ class Bookmark(models.Model):
     url = models.URLField('URL', unique=True)
     time = models.DateTimeField('Time', null=True, blank=True)
     date = models.DateField('date', default=datetime.datetime.now, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return "%s %s" %(self.title, self.url)
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from bookmark.views import BookmarkLV, BookmarkDV
+from bookmark.views import *
 
 app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkLV.as_view(), name='index'),
     path('<int:pk>/', BookmarkDV.as_view(), name='detail'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('change/', BookmarkChangeLV.as_view(), name='change'),
+    path('<int:pk>/update/', BookmarkUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', BookmarkDeleteView.as_view(), name='delete'),
 ]
